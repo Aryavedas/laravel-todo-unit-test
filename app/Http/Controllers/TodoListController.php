caffc8a0183658e5e9ae16ca88530534f627d73c
aryaveda <aryaveda@192.168.1.7>
2025-07-03 22:34:35 +0700

    membuat controller todolist

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoListController extends Controller
+{
+    public function todolist(Request $request) {}
+
+    public function addTodo(Request $request) {}
+
+    public function removeodo(Request $request, $id) {}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\TodoListController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\OnlyGuestMiddleware;
 use App\Http\Middleware\OnlyMemberMiddleware;
@@ -23,4 +24,10 @@ Route::controller(UserController::class)->group(function () {
     Route::get('/login', 'login')->middleware([OnlyGuestMiddleware::class]);
     Route::post('/login', 'doLogin')->middleware([OnlyGuestMiddleware::class]);
     Route::post('/logout', 'doLogout')->middleware(OnlyMemberMiddleware::class);
+});
+
+Route::controller(TodoListController::class)->middleware([OnlyMemberMiddleware::class])->group(function () {
+    Route::get("/todolist", "todolist");
+    Route::post("/todolist", "addTodo");
+    Route::post("/todolist/{id}/delete", "removeTodo");
 });
